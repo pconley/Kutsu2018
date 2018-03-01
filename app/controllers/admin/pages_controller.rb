925314a76fc451afc755e082da0a2139b1916b06
Pat Conley <pconley312@gmail.com>
2018-03-01 08:12:49 -0500

    update notes

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -15,8 +15,8 @@ class Admin::PagesController < ApplicationController
   def notes
   	puts "*** Admin::PagesController::notes"
   	notes = [
-      Note.new(Date.new(2018,2,26),"security fixes","well not really!"),
-      Note.new(Date.new(2018,2,24),"performance fixes","well not really any faster!"),
+      Note.new(Date.new(2018,3,1),"admin pages","sign in/out using devise to access the admin pages... only a quotes administration page really functional"),
+      Note.new(Date.new(2018,2,24),"initial release","merge a template with a standard rails app"),
   	]
     @notes = notes
         .sort { |a,b| b.released_on <=> a.released_on }
